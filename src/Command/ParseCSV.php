7b2b273480c20d5a82b634c118f22231f2a2cd5b
Theo VAUVILLIERS <theo.vauvilliers@synolia.com>
2023-01-24 22:40:33 +0100

    [FEATURE] Creation of the logic to create the stories on Shortcut

--- src/Command/ParseCSV.php
+++ src/Command/ParseCSV.php
@@ -21,14 +21,16 @@ class ParseCSV extends Command
     // CSV => SHORCUT
     protected const MAPPING = [
         'description' => 'name',
-        'status' => '',
+        'status' => 'workflow_state_id',
         'epic' => 'epic_id',
-        'blocked by' => ''
+        'blocked by' => 'description' // TODO: CHANGE THIS !
     ];
     protected bool $requireFileName = true;
+    protected ?ConnectorShortcut $connector = null;
 
     public function __construct(string $name = null)
     {
+        $this->connector = new ConnectorShortcut();
         parent::__construct($name);
     }
 
@@ -53,33 +55,75 @@ class ParseCSV extends Command
             throw new \LogicException('This command accepts only an instance of "ConsoleOutputInterface".');
         }
 
-        $connector = new ConnectorShortcut();
-
         // Transform csv in array
         $csv = new Csv();
-        $data = $csv->read($input->getArgument('csv-name'));
-        $data = $csv->headerAsAssocArray($data);
+        $rows = $csv->read($input->getArgument('csv-name'));
+        $rows = $csv->headerAsAssocArray($rows);
+
+        // Get project_id
+        $project = $this->connector->call('GET', 'projects');
+        $projectId = current($project)['id'];
+        unset($project);
+
+        // Get workflow
+        $workfows = $this->connector->call('GET', 'workflows');
+
+        // Extract states and create an associative array
+        $states = array_reduce(current($workfows)['states'], function ($states, $state) {
+            $states[$state['name']] = $state['id'];
+            return $states;
+        }, []);
+        unset($workfows);
 
         // Get epics and create an associative array
-//        $epics = $connector->call('GET', 'epics');
-//        $epics = array_reduce($epics, function ($epics, $epic) {
-//            $epics[$epic['name']] = $epic['id'];
-//            return $epics;
-//        }, []);
-//
-//        // TODO: Insert data on shortcut App
-//        // Get project_id
-//        $project = $connector->call('GET', 'projects');
-//        $project_id = current($project)['id'];
-//
-//        $data = [
-//            'name' => 'Story from CLI',
-//            'project_id' => $project_id,
-//        ];
-//
-//        $story = $connector->call('POST', 'stories', $data, 'json');
-//        var_dump($story);
+        $epics = $this->connector->call('GET', 'epics');
+        $epics = array_reduce($epics, function ($epics, $epic) {
+            $epics[$epic['name']] = $epic['id'];
+            return $epics;
+        }, []);
+
+        // Generate an array containing all the information to push to the API
+        $data = array_map(function ($row) use ($epics, $states, $projectId) {
+            $row = array_combine(self::MAPPING, $row);
+
+            foreach ($row as $key => $value) {
+                // Epic name to epic id
+                $row[$key] = $this->replaceNameById($key, $value, $row, $epics);
+                // State name to State id
+                $row[$key] = $this->replaceNameById($key, $value, $row, $states);
+            }
+
+            $row['project_id'] = $projectId;
+
+            return $row;
+        }, $rows);
+
+        foreach ($data as $row) {
+            try {
+                $story = $this->connector->call('POST', 'stories', $row, 'json');
+                $output->writeln([
+                    'The story ' . $row['name'] . ' has been created',
+                ]);
+            } catch (\Throwable $t) {
+                $output->writeln([
+                    'Impossible to create this story : ' . $row['name'],
+                    'Error : ' . $t->getMessage(),
+                ]);
+            }
+        }
 
         return Command::SUCCESS;
     }
+
+    protected function replaceNameById(string $key, string $value, array $row, array $data): string
+    {
+        // If the value exists on the application, returns the corresponding id, otherwise returns its own value
+        if (in_array($value, array_keys($data))) {
+            return $data[$value];
+        }
+
+        return $row[$key];
+    }
+
+    // TODO: Rearrange the stories in the correct order to be able to create the "is blocked by" relationships
 }
